b626d53526d3fc1927e6b9afaa4544334611e08c
ashraf <ashrafawad2@gmail.com>
2022-01-16 14:43:45 +0200

    Add Last Touch for style and Names

--- js/main.js
+++ js/main.js
@@ -14,3 +14,37 @@ scrollToTop.onclick = function() {
     behavior: "smooth"
   })
 }
+
+let ourSkills = document.getElementById('our-skills');
+let progress = document.querySelectorAll('.progress span');
+
+window.onscroll = function() {
+  if(window.scrollY >= ourSkills.offsetTop - 300) {
+    progress.forEach((span) => {
+      span.style.width = span.dataset.width;
+    })
+  }
+}
+
+// let section = document.getElementById('stats');
+// let numbers = document.querySelectorAll('.card .number');
+// let started = false;
+
+// window.onscroll = function() {
+//   if(window.scrollY >= section.offsetTop - 100) {
+//     if(!started) {
+//       numbers.forEach((number) => startCount(number));
+//     }
+//     started = true;
+//   }
+// }
+
+// function startCount(e) {
+//   let goal = e.dataset.goal;
+//   let counter = setInterval(() => {
+//     e.textContent++;
+//     if(e.textContent == goal) {
+//       clearInterval(counter);
+//     }
+//   }, 2000 / goal)
+// }
